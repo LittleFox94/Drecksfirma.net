d2d158b769e6a7c60aec6da4550faddf8fd76039
Moritz Grosch (LittleFox) <littlefox@fsfe.org>
2014-10-23 00:28:08 +0200

    Added text to the startpage

--- urlhandlerstartpage.cpp
+++ urlhandlerstartpage.cpp
@@ -2,5 +2,11 @@
 
 void URLHandlerStartpage::run(Template* tpl, cgicc::Cgicc* cgi)
 {
-	tpl->setTitle("Highscore - ");
+	tpl->setTitle("");
+	tpl->setContent("<h3>Willkommen auf Drecksfirma.net</h3>"
+		"<p>Hier wird basisdemokratisch darüber abgestimmt, welche Firma die allerschlimmste ist.</p>"
+		"<h4>So funktionierts:</h4>"
+		"<p>Sie ärgern sich gerade über eine Firma? Dann schnell im aktuellen Highscore für diese Firma abstimmen. "
+		"Gibt es diese Firma im Highscore noch nicht, muss diese erst hinzugefügt werden - damit ist dann automatisch abgestimmt.</p>"
+		"<p>Die Zähler werden jeden Monat zurückgesetzt, die schlimmste Firma des Monats landet jeweils in der Hall of Shame.</p>");
 }
